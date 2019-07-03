a566df2de883390d2d72176a12bb9750a8e593a0
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-07-03 10:42:08 +0000

    allowed hosts updated for AWS migration

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-#import env
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), "issue-tracker-jw.herokuapp.com"]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), "issue-tracker-jw.herokuapp.com","274ae44148d146969ff56f26acbaa683.vfs.cloud9.us-east-1.amazonaws.com"]
 
 
 # Application definition
