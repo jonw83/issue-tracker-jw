6145db4e1580716c232a35d152eb3dc01c65cd46
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-07-24 18:40:59 +0000

    upvote url added

--- issues/urls.py
+++ issues/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_issues, add_issue
+from .views import all_issues, add_issue, upvote
 
 urlpatterns = [
     url(r'^$', all_issues, name = 'issues'),
     url(r'^addissue/$', add_issue, name = 'addissue'),
+    url(r'^(?P<pk>\d+)/upvote/$', upvote, name='upvote'),
     ]
 
 
