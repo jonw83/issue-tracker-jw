886adb87f728e2bdcef07a4fa2a0acbb1789f1c1
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-07-25 19:57:14 +0000

    added view to remove items from cart

--- cart/views.py
+++ cart/views.py
@@ -19,19 +19,13 @@ def add_to_cart(request, id):
     request.session['cart'] = cart
     return redirect(reverse('index'))
 
-
-def adjust_cart(request, id):
-    """
-    Adjust the quantity of the specified issue to the specified
-    amount
-    """
-    quantity = int(request.POST.get('quantity'))
+def remove_item_from_cart(request, id):
     cart = request.session.get('cart', {})
-
-    if quantity > 0:
-        cart[id] = quantity
-    else:
-        cart.pop(id)
-    
+    cart.pop(id)
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+    
+    
+    
+    
+    
