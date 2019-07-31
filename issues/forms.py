93b6c6e18621f0cb12075541b4ea30cdf3497854
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-07-31 19:31:40 +0000

    added EditIssueForm class

--- issues/forms.py
+++ issues/forms.py
@@ -6,4 +6,9 @@ class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
         fields = ('title', 'issue_type', 'description')
+        
+class EditIssueForm(forms.ModelForm):
+    class Meta:
+        model = Issue
+        fields = ('title', 'issue_type', 'description', 'requested_by', 'date_created', 'status', 'completed_date')
 
