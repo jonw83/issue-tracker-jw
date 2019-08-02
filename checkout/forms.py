7c9735cad3ced7f1ed4088f10d7673700fdd5316
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-08-02 18:39:38 +0000

    increased range to 13 in month choices so 12 can be selected

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,8 +4,8 @@ from .models import Order
 
 class MakePaymentForm(forms.Form):
 
-    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
-    YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
+    YEAR_CHOICES = [(i, i) for i in range(2019, 2031)]
 
     credit_card_number = forms.CharField(label='Credit card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)', required=False)
