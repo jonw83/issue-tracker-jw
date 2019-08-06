8bc1d1a99bf1cc74449744737ec8a59b70d7f103
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-08-06 19:48:10 +0000

    removed date from Order model

--- checkout/models.py
+++ checkout/models.py
@@ -11,8 +11,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
-    date = models.DateField()
-
+ 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
 
