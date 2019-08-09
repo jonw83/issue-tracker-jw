6cca12b40fbdb2386bd8ce3381b061246c78a7c2
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-08-09 18:42:52 +0000

    test written and run for issues model

--- issues/tests.py
+++ issues/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
 from .models import Issue
+from django.contrib.auth.models import User
 
 # Create your tests here.
-class IssueTests(TestCase):
+class IssueModelTest(TestCase):
     
     """
-    Test for issue title
+    Test for adding and issue
     """
-    
-    def test_str(self):
-        test_name = Issue(title = "issue one")
-        self.assertEqual(str(test_name), "issue one")
+
+    def test_issue(self):
+        user = User()
+        issue = Issue(title = "Test Issue Title", issue_type = "Feature", description = "Test Issue Description", requested_by = user, status = "Not Started", price = 99.99)
+        self.assertEqual(issue.title, "Test Issue Title")
+        self.assertEqual(issue.description, "Test Issue Description")
+        self.assertEqual(issue.requested_by, user)
+        self.assertEqual(issue.price, 99.99)
+        self.assertEqual(issue.issue_type, "Feature")
+        self.assertEqual(issue.status, "Not Started")
+      
