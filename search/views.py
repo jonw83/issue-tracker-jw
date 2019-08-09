fcb99ff3445bf15499e2646a57c886241bb0c297
Ubuntu <ubuntu@ip-172-31-92-68.ec2.internal>
2019-08-09 15:44:23 +0000

    added view function description

--- search/views.py
+++ search/views.py
@@ -4,5 +4,6 @@ from issues.models import Issue
 # Create your views here.
 
 def do_search(request):
+    """Search for an issue"""
     issues = Issue.objects.filter(title__icontains=request.GET['q'])
     return render(request, "issues.html", {"issues": issues})
